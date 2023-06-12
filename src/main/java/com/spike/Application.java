b2bf42ff62af37f3d7d245bf8d2810cfba358629
jjc387 <jjc387@cornell.edu>
2023-06-12 16:18:35 -0700

    first commit

---
+++ src/main/java/com/spike/Application.java
@@ -0,0 +1,11 @@
+package com.spike;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/com/spike/algoTesting.java
@@ -0,0 +1,7 @@
+package com.spike;
+
+public class algoTesting {
+  public void runAlgo(){
+    
+  }
+}
